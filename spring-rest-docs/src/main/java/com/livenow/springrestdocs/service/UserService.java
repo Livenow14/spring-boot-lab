b95b4a95fd6909db0699bb45ab32579ce1c09f78
livenow14 <xowjd41@naver.com>
2021-11-07 21:12:01 +0900

    feat: Spring Rest Docs 예제 코드 작성

---
+++ spring-rest-docs/src/main/java/com/livenow/springrestdocs/service/UserService.java
@@ -0,0 +1,13 @@
+package com.livenow.springrestdocs.service;
+
+import com.livenow.springrestdocs.controller.dto.UserRequest;
+import com.livenow.springrestdocs.controller.dto.UserResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public UserResponse signUp(UserRequest userRequest) {
+        return null;
+    }
+}
